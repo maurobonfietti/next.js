6fcb2a06aecaf5f9e24bfb409dc0a8abcee046ff
Hendrik Liebau <mail@hendrik-liebau.de>
2025-05-28 13:10:34 +0200

    Fix test for dev mode

--- test/e2e/app-dir/empty-fallback-shells/empty-fallback-shells.test.ts
+++ test/e2e/app-dir/empty-fallback-shells/empty-fallback-shells.test.ts
@@ -55,9 +55,8 @@ describe('empty-fallback-shells', () => {
             '/with-cached-io/with-suspense/params-in-page/foo'
           )
 
-          const layoutDateRouteShell = $(
-            '[data-testid="layout-buildtime"]'
-          ).text()
+          const selector = `[data-testid="layout-${isNextDev ? 'runtime' : 'buildtime'}"]`
+          const layoutDateRouteShell = $(selector).text()
 
           // Sanity check that we've selected the correct element.
           expect(layoutDateRouteShell).toStartWith('Layout:')
@@ -67,9 +66,7 @@ describe('empty-fallback-shells', () => {
             '/with-cached-io/with-suspense/params-in-page/bar'
           )
 
-          const layoutDateFallbackShell = $(
-            '[data-testid="layout-buildtime"]'
-          ).text()
+          const layoutDateFallbackShell = $(selector).text()
 
           expect(layoutDateFallbackShell).toBe(layoutDateRouteShell)
         })
